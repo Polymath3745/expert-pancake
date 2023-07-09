febbb2d4bb9876fc6891cb064ebbf83d47790efe
Gabriel <gabriel@gabriel-System-Product-Name>
2023-07-09 14:01:45 -0700

    implemented the raise for adding errors

--- rand/student.py
+++ rand/student.py
@@ -1,28 +1,22 @@
 class Student:
     def __init__(self, name, house):
+        if not name:
+            raise ValueError
         self.m_name = name
         self.m_house = house
 
-    def check_house(self):
-        houses = ["Gryfindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
-        if (self.m_house in houses):
-            print("passed")
-        else:
-            print("fail")
     
-
-    # place holder
-
 def main():
     student = get_student()
-    student.check_house()
-    print(type(student.m_house))
     print(f"{student.m_name} from {student.m_house}")
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return Student(name, house)
+    try:
+        return Student(name, house)
+    except ValueError:
+        ...
 
 if __name__ == '__main__':
     main()
