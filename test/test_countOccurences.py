2f1139b33de598f992dc8cc8763fa184e9fcf596
Gabriel <gabriel@gabriel-System-Product-Name>
2023-07-10 14:32:52 -0700

    consolidated all related list problems in one file

--- test/test_countOccurences.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from countOccurences import count_occurences
-
-def test_count_occurences():
-    assert count_occurences([1,1,1,2,3,4,5,6,7], 1) == 3
-    assert count_occurences([1,1,1,2,3,4,5,6,7], 3) == 1
