f6fb39709122e629c5c015a8e0cd58617062a751
Gabriel <gabriel@gabriel-System-Product-Name>
2023-07-17 16:20:38 -0700

    created another  gitignore

--- rand/functions_vars.py
+++
@@ -1,6 +0,0 @@
-def main():
-    name  = input("What's your name? ")
-    print("hello " + name)
-
-if __name__ == '__main__':
-    main()
--- test/test_evenSum.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from evenSum import calculate_even_sum
-
-def test_calculate_even_sum():
-    assert calculate_even_sum(10) == 30
-    assert calculate_even_sum(5) == 6
-    assert calculate_even_sum(0) == 0
