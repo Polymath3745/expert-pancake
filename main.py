0a85eef29f16a13bde62205431a2655dbcbeff3c
Gabriel <gabriel@gabriel-System-Product-Name>
2023-07-18 15:35:44 -0700

    cleaned new branch

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-from helpers.calculator import Calculator
 
 def main():
-    arg1 = int(input("Enter value1: "))
-    arg2 = int(input("Enter value2: "))
+    print("hello world")
 
-    calc = Calculator(arg1, arg2)
-    x = calc.x
-    y = calc.y
-
-    print(f"The two arguments given: {x}, {y}")
-
+    
 if __name__ == '__main__':
     main()
 
